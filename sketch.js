9319a75e09c807335b03e4b84498d2789c31955a
ksycip <56284512+ksycip@users.noreply.github.com>
2021-12-11 23:19:18 -0800

    almost there
    need to align text, and fill in 'about' section. Add more interesting elements.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,39 @@
-// function setup() {
-//   createCanvas(400, 400);
-// }
-//
-// function draw() {
-//   background(220);
-// }
+let array = [];
+let strokeWidth = 5;
+let noiseOffset = 0.0;
+let colorPicker;
+let song;
+
+// colorPicker
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+    colorPicker = createColorPicker('#ff0000');
+    colorPicker.position(1370, 10);
+  song = loadSound('assets/pop.wav');
+
+}
+
+// drawing tool
+function draw() {
+  strokeWeight(5);
+  stroke(colorPicker.color());
+  if (mouseIsPressed === true) {
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
+}
+
+
+// space bar for clear
+function keyPressed() {
+  if (key == ' '){
+  clear();
+  }
+}
+
+function mousePressed(){
+  if (song.isPlaying()) {
+    song.stop();
+  } else {
+    song.play();
+  }
+}
